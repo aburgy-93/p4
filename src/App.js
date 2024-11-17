0b0bc24f10e945fc3bb833a668d3001f9d0724f9
aburgy-93 <aburgy@uwalumni.com>
2024-11-17 11:46:12 -0600

    modified App.js to have my name in it

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 function App() {
     return (
         <div className="App">
-            <h1>Hello Madison, WI!!!!</h1>
+            <h1>Alex Burgy</h1>
         </div>
     );
 }
